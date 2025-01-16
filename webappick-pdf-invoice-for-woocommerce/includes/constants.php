a117575a8d756b61938a7cce93665f5343c6abaa
Sean Murphy <sean@iamseanmurphy.com>
2025-01-16 05:17:20 +0000

    3.7.40

--- webappick-pdf-invoice-for-woocommerce/includes/constants.php
+++ webappick-pdf-invoice-for-woocommerce/includes/constants.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'CHALLAN_FREE_VERSION', '3.7.39' );
+define( 'CHALLAN_FREE_VERSION', '3.7.40' );
 
 if ( ! defined( 'CHALLAN_FREE_FONTS_COUNT' ) ) {
     /**
